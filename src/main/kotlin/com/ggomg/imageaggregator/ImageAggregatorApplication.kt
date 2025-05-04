e3b2433445e4d2f0876fc5fda54c13ac4b098494
wjh2012 <wjh2012@naver.com>
2025-05-04 14:27:40 +0900

    Init

---
+++ src/main/kotlin/com/ggomg/imageaggregator/ImageAggregatorApplication.kt
@@ -0,0 +1,11 @@
+package com.ggomg.imageaggregator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageAggregatorApplication
+
+fun main(args: Array<String>) {
+    runApplication<ImageAggregatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/ggomg/imageaggregator/ImageAggregatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ggomg.imageaggregator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImageAggregatorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
