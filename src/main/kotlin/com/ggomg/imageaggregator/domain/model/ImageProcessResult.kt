bacda05f23757eab37185b98e47c90500b6fc65a
wjh <wjh2012@naver.com>
2025-05-18 23:24:34 +0900

    ImageProcessResult

---
+++ src/main/kotlin/com/ggomg/imageaggregator/domain/model/ImageProcessResult.kt
@@ -0,0 +1,20 @@
+package com.ggomg.imageaggregator.domain.model
+
+data class ImageProcessResult(
+    val gid: String,
+    var ocrResult: OcrResult? = null,
+    var validationResult: ValidationResult? = null,
+    var thumbnailResult: ThumbnailResult? = null
+) {
+    fun applyOcrResult(result: OcrResult) {
+        this.ocrResult = result
+    }
+
+    fun applyValidationResult(result: ValidationResult) {
+        this.validationResult = result
+    }
+
+    fun applyThumbnailResult(result: ThumbnailResult) {
+        this.thumbnailResult = result
+    }
+}
